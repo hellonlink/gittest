7eb40220e156cfeac2b45e890293a7cd2fa67a61
jyb <0sunandmoon@gmail.com>
2021-01-08 21:50:11 +0800

    第五次提交，类库放入统一仓库文件夹下

---
+++ ComLib/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComLib
+{
+    /// <summary>
+    /// 这是类库
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public string Sex { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace test
         static void Main(string[] args)
         {
             ComLib.Person p = new ComLib.Person();
+            p.Sex = "男";
+            p.Age = 25;
+            p.Name = "张三";
             Console.WriteLine("哈哈搜狐佛说法");
             Console.ReadKey();
 
